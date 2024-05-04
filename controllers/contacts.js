580c894f6956e648733794623201b2b14142e558
Pritam96 <pritammondal96@gmail.com>
2024-05-04 10:02:15 +0530

    Creating async/await middleware

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,54 +1,43 @@
 const ContactModel = require("../models/Contact");
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 
 // @desc    Get all contacts
 // @route   GET /api/contacts
 // @access  Private
-exports.getContacts = async (req, res, next) => {
-  try {
-    const contacts = await ContactModel.find();
-    res.status(200).json({
-      success: true,
-      count: contacts.length,
-      data: contacts,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.getContacts = asyncHandler(async (req, res, next) => {
+  const contacts = await ContactModel.find();
+  res.status(200).json({
+    success: true,
+    count: contacts.length,
+    data: contacts,
+  });
+});
 
 // @desc    Get single contact
 // @route   GET /api/contacts/:id
 // @access  Private
-exports.getContact = async (req, res, next) => {
-  try {
-    const contact = await ContactModel.findById(req.params.id);
-    if (!contact) {
-      return next(
-        new ErrorResponse(`Contact not found with id of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({
-      success: true,
-      data: contact,
-    });
-  } catch (error) {
-    next(error);
+exports.getContact = asyncHandler(async (req, res, next) => {
+  const contact = await ContactModel.findById(req.params.id);
+  if (!contact) {
+    return next(
+      new ErrorResponse(`Contact not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({
+    success: true,
+    data: contact,
+  });
+});
 
 // @desc    Create new contact
 // @route   POST /api/contacts
 // @access  Private
-exports.createContact = async (req, res, next) => {
-  try {
-    const contact = await ContactModel.create(req.body);
+exports.createContact = asyncHandler(async (req, res, next) => {
+  const contact = await ContactModel.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: contact,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: contact,
+  });
+});
